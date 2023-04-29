de98697a20aa9d602db1c041e0542f292323292a
unknown <petarmancic@elfak.rs>
2023-04-29 13:50:06 +0200

    294

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from "./Login/Login";
+
+function App() {
+  return (
+    <main className="App">
+      <Login> </Login>
+      
+    </main>
+  );
+}
+
+export default App;
