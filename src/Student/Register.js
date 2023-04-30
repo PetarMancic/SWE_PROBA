cd1c93628f6d254f8011759b4ec00b8dd333faf9
Djole <djordje.glisic@elfak.rs>
2023-04-30 11:57:26 +0200

    Djole

--- src/Student/Register.js
+++ src/Student/Register.js
@@ -1,48 +1,70 @@
 import React,{useState} from "react";
 import Slika from "../images/index1.png";
-
-const [user, setUser] = useState('');
-    const [pwd, setPwd] = useState('');
-
-const handlerSubmit= ()=>
-{
-
-}
+import './Register.css';
 
 
 const Register = (props) => {
     return (
-    <div>
-        <form className="login-form" onSubmit={handlerSubmit}>
-                    <img className="index" src={Slika}></img>
-                    <h1>Welcome to E-index</h1>
-                    <h2>Login</h2>
+        <form className="login-form" >
+        <img className="index" src={Slika}></img>
+        <h1>Register to E-index</h1>
+        <h2>Please enter your data</h2>
 
-                    <label htmlFor="username">Username</label>
-                    <input
-                        id="username"
-                        type="text"
-                      //  value={user}
-                        onChange={userHandler}
-                    />
-                    <label htmlFor="pwd"> Password</label>
-                    <input
-                        id="password"
-                        type="password"
-                        value={pwd}
-                        onChange={passwordHandler}
-                    />
-                     <input
-                        id="password"
-                        type="password"
-                        value={pwd}
-                        onChange={passwordHandler}
-                    />
-                    <button type="submit"> Login</button>
+        <label htmlFor="index">Index</label>
+        <input
+            id="index"
+            type="number"
+        
+            className="inpuT"
+            
+          
+        />
+        <label htmlFor="jmbg"> JMBG</label>
+        <input
+            id="jmbg"
+            type="number"
+            className="inpuT"
+        />
+        <label htmlFor="username"> Username</label>
+        <input
+            id="username"
+            type="text"
+            className="inpuT"
+           
+        />
+        <label htmlFor="alternative"> Alternative E-mail</label>
+        <input
+            id="alternative"
+            type="email"
+            className="inpuT"
+           
+        />
+        <label htmlFor="pwd"> Password</label>
+        <input
+            id="password"
+            type="password"
+            className="inpuT"
+           
+        />
+        <label htmlFor="pwd"> Confirm your password</label>
+        <input
+            id="password"
+            type="password"
+            className="inpuT"
+           
+        />
+        
+        
+        
         
 
-        </form>
-    </div>
+        <button type="submit"> Register now</button>
+
+
+
+
+    </form>
+
     )
 }
 
